c7c4673ab663e73895da03d4d425e43960215046
Benjamin Schröder <Benjamin.Schroeder@haw-hamburg.de>
2023-01-12 19:40:43 +0100

    small refactor, improved comments

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import raylib as rl
 from pyray import *
 import json
 
-DISTANCE_X, DISTANCE_Y = 500, 200  # distances inbetween neurons
+# configuration
+DISTANCE_X, DISTANCE_Y = 400, 200  # distances inbetween neurons
 MULT_CONN = 10  # factor by which line thickness for connections is multiplied
 RADIUS = 50  # size/radius of each neuron
 FONT_SIZE = 16  # font size
 
 
 def main():
-    with open("../../PyCharmProjects/nnvisualizer/input.json") as file:
+    # read input file
+    with open("input.json") as file:
         data = json.load(file)
 
     # add all layers to a list (to simplify iteration later on)
@@ -18,13 +20,15 @@ def main():
         layers.append(hidden)
     layers.append(data["OutputLayer"])
 
-    init_window(1600, 900, "Window")
-    set_target_fps(0)
-
+    # determine size of the biggest layer (used to center smaller layers properly)
     max_layer = 0
     for layer in layers:
         neurons = layer["NeuronList"]
         max_layer = max(max_layer, len(neurons))
+
+    # window settings
+    init_window(1600, 900, "NN Visualizer")
+    set_target_fps(0)
 
     while not window_should_close():
         begin_drawing()
@@ -37,14 +41,17 @@ def main():
             for y, neuron in enumerate(neurons):
                 connected_list = neuron["ConnectedNeurons"]
                 connected_count = len(connected_list)
-                # draw connections to neurons of previous layer
+                # draw connections to neurons in previous layer
                 for y_prev, connected in enumerate(connected_list):
-                    # figure out starting points for current connection
+                    # figure out starting position for current connection
                     pos_x = 100 + RADIUS + DISTANCE_X * (x - 1)
+                    # last part of sum: center connections according to the biggest layer
                     pos_y = 100 + RADIUS + (DISTANCE_Y * y_prev) + ((max_layer - connected_count) / 2) * DISTANCE_Y
                     start = Vector2(pos_x, pos_y)
-                    # figure out ending points for current connection
+
+                    # figure out ending position for current connection
                     pos_x = 100 + RADIUS + DISTANCE_X * x
+                    # last part of sum: center connections according to the biggest layer
                     pos_y = 100 + RADIUS + (DISTANCE_Y * y) + ((max_layer - neuron_count) / 2) * DISTANCE_Y
                     end = Vector2(pos_x, pos_y)
 
@@ -58,6 +65,7 @@ def main():
                         thick = weight * MULT_CONN
                         color = SKYBLUE
 
+                    # actually draw line
                     rl.DrawLineEx(start, end, thick, color)
 
         # draw neurons for entire network
@@ -66,17 +74,21 @@ def main():
             neuron_count = len(neurons)
             # draw neurons in current layer
             for y, neuron in enumerate(neurons):
-                bias = neuron["Bias"]
-                value = neuron["Value"]
+                # figure out position for current neuron
                 pos_x = 100 + RADIUS + DISTANCE_X * x
+                # last part of sum: center neurons according to the biggest layer
                 pos_y = 100 + RADIUS + (DISTANCE_Y * y) + ((max_layer - neuron_count) / 2) * DISTANCE_Y
                 center = Vector2(pos_x, pos_y)
+
+                # draw neuron
                 rl.DrawCircleV(center, RADIUS, WHITE)
                 rl.DrawCircleV(center, 0.9 * RADIUS, BLACK)
+
+                # draw text below current neuron
+                bias = neuron["Bias"]
+                value = neuron["Value"]
                 rl.DrawText(str.encode("Value: {}\nBias: {}".format(round(value, 3), round(bias, 3))),
                             int(pos_x - RADIUS), int(pos_y + RADIUS + 10), FONT_SIZE, WHITE)
-                # text, x, y, size, color
-
         end_drawing()
     close_window()
 
